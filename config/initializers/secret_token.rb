2f4e97e424d84cad3c55271f7adb022ec23fc389
Thim Warfvinge <thiwa739@gmail.com>
2013-02-15 09:10:46 +0100

    something

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaptureVideo::Application.config.secret_token = '039ec61c33e3396e1dc980b4e5ec4e73838896f5ae911f48e2e49e7a954efeb7b5c08ebfb1d62c260134cca747f1037b50d3fa85dd92380d9b3570e267747b00'
